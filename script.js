dc239db18cabf566f69af0c8b1562c3972f84244
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-29 22:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+
+function changeURL(url) {
+	var url = "https://localhost:8080/";
+var name = document.getElementById("name").innerText;
+var year = document.getElementById("year").innerText;
+	console.log(name,year);
+	document.getElementById("url").innerHTML = `${url}?name=${name}&year=${year}/`;
+}
